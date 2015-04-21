d82a96269cc666e0d7311a590caf67c4e519133e
Julia Ting <julia.ting@gatech.edu>
2015-04-20 22:06:53 -0400

    Renamed to PopularBookReport and added PopularSubjectReport

--- PopularSubjectReport.java
+++ PopularSubjectReport.java
@@ -20,7 +20,7 @@ import javafx.scene.paint.Paint;
 import javafx.scene.layout.BorderStrokeStyle;
 import javafx.scene.layout.CornerRadii;
 
-public class PopularReport extends GridPane {
+public class PopularSubjectReport extends GridPane {
     private final Main app;
 
     private Label title1, title2, title3,
@@ -29,7 +29,7 @@ public class PopularReport extends GridPane {
             checkout4, checkout5, checkout6;
 
 
-    public PopularReport(Main app) {
+    public PopularSubjectReport(Main app) {
         this.app = app;
 
         setAlignment(Pos.CENTER);
@@ -37,14 +37,14 @@ public class PopularReport extends GridPane {
         setVgap(10);
         setPadding(new Insets(15, 15, 15, 15));
 
-        Label mainTitle = new Label("Popular Books Report");
+        Label mainTitle = new Label("Popular Subject Report");
         HBox hbMainTitle = new HBox();
         hbMainTitle.setAlignment(Pos.CENTER);
         hbMainTitle.getChildren().add(mainTitle);
         add(hbMainTitle, 0, 0, 3, 1);
 
         Label month = new Label("Month");
-        Label title = new Label("Title");
+        Label title = new Label("Top Subject");
         Label checkout = new Label("Checkouts");
         Label jan = new Label("January");
         Label feb = new Label("February");
@@ -81,26 +81,16 @@ public class PopularReport extends GridPane {
         vbCheckout.getChildren().addAll(checkout, checkout1, checkout2, checkout3,
                                      checkout4, checkout5, checkout6);
 
-        // add(title1, 2, 2);
-        // add(title2, 2, 3);
-        // add(title3, 2, 4);
-        // add(title4, 2, 5);
-        // add(title5, 2, 6);
-        // add(title6, 2, 7);
+      
         add(vbTitle, 1, 1);
         add(vbCheckout, 2, 1);
-        // add(checkout1, 3, 2);
-        // add(checkout2, 3, 3);
-        // add(checkout3, 3, 4);
-        // add(checkout4, 3, 5);
-        // add(checkout5, 3, 6);
-        // add(checkout6, 3, 7);
+       
 
 
     }
 
     public static Scene makeScene(Main app) {
-        return new Scene(new PopularReport(app));
+        return new Scene(new PopularSubjectReport(app));
     }
 }
 
